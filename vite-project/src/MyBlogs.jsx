b787c597babbb5157ccf9f17d295a11b05fc13e7
Henry <manojlostanarevic@gmail.com>
2025-06-17 18:29:17 +0200

    ui done

--- vite-project/src/MyBlogs.jsx
+++ vite-project/src/MyBlogs.jsx
@@ -1,7 +1,15 @@
 function MyBlogs() {
   return(
   <div className="my-blogs">
-
+    <h2>MyBlogs</h2>
+    <div className="kontejner">
+      <div className='mala-vijest'></div>
+      <div className='mala-vijest'></div>
+      <div className='mala-vijest'></div>
+      <div className='mala-vijest'></div>
+      <div className='mala-vijest'></div>
+      <div className='mala-vijest'></div>
+    </div>
   </div>
   )
 }
