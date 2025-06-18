c7a80ed6996adf1ac608c6841140f2c3f32c16d3
Henry <manojlostanarevic@gmail.com>
2025-06-18 17:18:18 +0200

    set up for my Blogs

--- vite-project/src/MyBlogs.jsx
+++ vite-project/src/MyBlogs.jsx
@@ -1,14 +1,19 @@
 function MyBlogs() {
+
+
+  
   return(
   <div className="my-blogs">
     <h2>MyBlogs</h2>
     <div className="kontejner">
-      <div className='mala-vijest'></div>
-      <div className='mala-vijest'></div>
-      <div className='mala-vijest'></div>
-      <div className='mala-vijest'></div>
-      <div className='mala-vijest'></div>
-      <div className='mala-vijest'></div>
+      <div className='mala-vijestt'>
+        <div className="naslovv"><h2>Jedem pitu brale</h2></div>
+      </div>
+      <div className='mala-vijestt'></div>
+      <div className='mala-vijestt'></div>
+      <div className='mala-vijestt'></div>
+      <div className='mala-vijestt'></div>
+      <div className='mala-vijestt'></div>
     </div>
   </div>
   )
